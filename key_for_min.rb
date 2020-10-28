6cc53c23197593d89eb3b0f0493e4ef676ce7e96
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-10-27 22:12:17 -0400

    testing

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,24 @@
 
 def key_for_min_value(name_hash)
 
-end
+    if name_hash.empty?
+        return nil
+    end 
+
+    min = nil 
+    key_of_smallest_value = []
+    
+    name_hash.each do |key, value|
+
+        if min == nil 
+            min = value
+            key_of_smallest_value = key
+        elsif value < min
+            min = value
+            key_of_smallest_value = key
+        end 
+    end 
+
+    return key_of_smallest_value  
+
+    end 
